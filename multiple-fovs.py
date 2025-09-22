668f916793482920e0bdba0db1242da5782e35a0
Alistair Curd <a.curd@leeds.ac.uk>
2025-09-22 15:50:46 +0100

    Add file.

---
+++ multiple-fovs.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Explore localisation data from multiple FOVs
+# -------------------------------------------------
+
+# ## IMPORTANT
+# ## Disable autosave for Jupytext version control with a paired .py script
+# ### But manually saving the notebook frequently is still good
+
+# %autosave 0
+
+# ## Imports
+
+# +
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+# -
+
+
